08ed0c8dce1b7060f53df7d6087bdbed6395dc0f
Tangui Le Pense <tanguilp@aplaut.com>
2021-11-07 19:24:53 +0300

    WIP

--- lib/plug_http_cache.ex
+++ lib/plug_http_cache.ex
@@ -1,18 +1,95 @@
-defmodule PlugHttpCache do
+defmodule PlugHTTPCache do
   @moduledoc """
   Documentation for PlugHttpCache.
   """
 
-  @doc """
-  Hello world.
+  require Logger
 
-  ## Examples
+  @behaviour Plug
 
-      iex> PlugHttpCache.hello()
-      :world
+  @impl true
+  def init(opts) do
+    unless opts[:http_cache][:store], do: raise "missing `:store` option for `:http_cache`"
+    unless opts[:http_cache][:type], do: raise "missing `:type` option for `:http_cache`"
 
-  """
-  def hello do
-    :world
+    opts
   end
+
+  @impl true
+  def call(conn, opts) do
+    case :http_cache.get(request(conn), opts[:http_cache]) do
+      {:ok, {resp_ref, response}} ->
+        send_cached(conn, resp_ref, response, opts)
+
+      {:stale, {resp_ref, response}} ->
+        send_cached(conn, resp_ref, response, opts)
+
+      _ ->
+        install_callback(conn, opts)
+    end
+  end
+
+  defp send_cached(conn, resp_ref, {_status, resp_headers, _body} = response, opts) do
+    :http_cache.notify_use(resp_ref, opts[:http_cache])
+
+    Logger.info([what: :cached_response_sent, which: url(conn)])
+
+    %Plug.Conn{conn | resp_headers: resp_headers}
+    |> do_send_cached(response)
+    |> Plug.Conn.halt()
+  end
+
+  defp do_send_cached(conn, {status, _resp_headers, {:sendfile, offset, length, path}}) do
+    Plug.Conn.send_file(conn, status, path, offset, length)
+  end
+
+  defp do_send_cached(conn, {status, _resp_headers, iodata_body}) do
+    Plug.Conn.send_resp(conn, status, iodata_body)
+  end
+
+  defp install_callback(conn, opts) do
+    Plug.Conn.register_before_send(conn, &cache_response(&1, opts))
+  end
+
+  @doc false
+  def cache_response(%Plug.Conn{state: :set} = conn, opts) do
+    case :http_cache.cache(request(conn), response(conn), opts[:http_cache]) do
+      {:ok, {status, resp_headers, resp_body}} ->
+        %Plug.Conn{conn | status: status, resp_headers: resp_headers, resp_body: resp_body}
+
+      :not_cacheable ->
+        Logger.info([what: :response_not_cached, which: url(conn), reason: :not_cacheable])
+
+        conn
+    end
+  end
+
+  def cache_response(conn, _opts) do
+    conn
+  end
+
+  defp request(conn) do
+    {
+      conn.method,
+      url(conn),
+      conn.req_headers,
+      req_body(conn)
+    }
+  end
+
+  defp response(conn) do
+    {
+      conn.status,
+      conn.resp_headers,
+      conn.resp_body
+    }
+  end
+
+  defp url(%Plug.Conn{query_string: ""} = conn),
+    do: "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}"
+  defp url(conn),
+    do: "#{conn.scheme}://#{conn.host}:#{conn.port}#{conn.request_path}?#{conn.query_string}"
+
+  defp req_body(%Plug.Conn{body_params: %Plug.Conn.Unfetched{}}), do: ""
+  defp req_body(conn), do: :erlang.term_to_binary(conn.body_params)
 end
--- mix.exs
+++ mix.exs
@@ -1,11 +1,11 @@
-defmodule PlugHttpCache.MixProject do
+defmodule PlugHTTPCache.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :plug_http_cache,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +21,9 @@ defmodule PlugHttpCache.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:http_cache, path: "../http_cache"},
+      {:plug, "~> 1.0"}
     ]
   end
 end
